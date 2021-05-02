b30c37773e383ff1551d6b742a3d0da7256a8134
dwikurniawan2074 <83540724+dwikurniawan2074@users.noreply.github.com>
2021-05-02 22:05:45 +0700

    findword1.cpp
    menambahkan fungsi getword vertical

---
+++ findword.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+const int cols = 16, rows = 15;
+
+ char words[rows][cols] = {"tgbwwinterwsesn",
+                                "aaunttmmhfoodnb",
+                                "jlwcqldzmpmvdmr",
+                                "asagmquwvvbsohi",
+                                "bwplotanadtpgnc",
+                                "rewngodjcpnatnk",
+                                "eeotwosbqharrsa",
+                                "azcgeswewnaknpb",
+                                "dinnerqodlwdcar",
+                                "onopkwmparktzcc",
+                                "qbfrogmamwpweey",
+                                "lqzqnnmrzjjsclg",
+                                "mosgzczetdbooto",
+                                "pdcrzmsngrdnrpz",
+                                "ohnkzwaterjgtra"};
+
+//char *getWordVertical(int);    //satria
+char *getWordVertical(int a){
+static char simpan [16];
+for (int i = 0;i<16;i++){
+    simpan [i] = words [i][a];
+}
+return simpan;
+}
+
+char *reverse(char *);         //nur
+bool searchVertical(char *);   //dwi
+bool searchHorizontal(char *); //
+
+
+int main()
+{
+    char word[16];
+    int n;
+    cin>>n;
+    for (int i=0;i<n;i++){
+        cin.getline(word,16,'\n');
+        if (searchVertical(word) || searchHorizontal(word))
+            cout << "Ada\n";
+        else 
+            cout << "Tidak Ada\n";
+    }
+    return 0;
+}
